f7d9c9be16512488f500ad127685c1764ca04fb2
tsuruta <tsuruta19951122@gmail.com>
2020-11-23 17:01:25 +0000

    post

--- app/User.php
+++ app/User.php
@@ -37,4 +37,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
     
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+    
+    //このユーザーに関係するモデルの件数をロードする
+     public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts');
+    }
+    
 }
