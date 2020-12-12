b8561b2c8d360dfbbac2756516d6c694f15f889c
tsuruta <tsuruta19951122@gmail.com>
2020-12-12 08:12:45 +0000

    Procfile

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -19,10 +19,10 @@
                         <ul class="dropdown-menu dropdown-menu-right">
                                 {{-- ユーザ詳細ページへのリンク --}}
                                 <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
-                                <li class="daaaaropdown-divider"></li>
+                                <li class="dropdown-divider"></li>
                                 {{-- ログアウトへのリンク --}}
                                 <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
-                                <li class="daaaaropdown-divider"></li>
+                                <li class="dropdown-divider"></li>
                                 {{-- お気に入りへのリンク --}}
                                 
                                 <li class="dropdown-item">{!! link_to_route('users.favorites', 'Favorites',['id' => $user->id]) !!}</li>
